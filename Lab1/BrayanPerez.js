63d7109f93a7d64ff0262562453545460e5013fe
Brayanperezballadares <bpballadares@gmail.com>
2023-04-01 15:25:03 -0600

    tarea 1

--- Lab1/BrayanPerez.js
+++ Lab1/BrayanPerez.js
@@ -1,1 +1,20 @@
-// Hola   guuulswkfdoedfhhh ggg
+function SumarNumerosDesdeCero(n)  //s O(n)/
+{
+    let counter = 0;
+    for(let i = 0; i <= n; i++)
+    {
+        counter = counter + i;
+    }
+
+    return counter;
+}
+
+console.log(SumarNumerosDesdeCero(100));
+
+
+
+function SumaGaussiana(n) // es O(1)
+{ 
+    return (n * (n + 1)) / 2;
+}
+//brayan tarea

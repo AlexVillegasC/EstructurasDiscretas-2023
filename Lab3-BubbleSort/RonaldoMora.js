8ce520faa8e7cfdfca48f2b3cd219ea78a0190e2
Ronaldo Mora <ronaldo.mora.diaz@est.una.ac.cr>
2023-04-13 18:28:41 -0600

    RonaldoMora lab 3

---
+++ Lab3-BubbleSort/RonaldoMora.js
@@ -0,0 +1,20 @@
+function SortGrades(arr) {
+  let swapped;
+
+  do {
+    swapped = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+}
+
+Grades = [88, 97, 75, 83, 72, 70, 90, 79, 84];
+const sortedArray = SortGrades(Grades);
+console.log("Mejores 5 calificaciones:", sortedArray.slice(0, 5));

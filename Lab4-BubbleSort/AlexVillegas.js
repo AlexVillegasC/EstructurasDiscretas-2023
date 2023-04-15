03eef785d5c88ac22d779d911dc94da9474ce330
Alex Villegas <avillegas@dragonspears.com>
2023-04-15 11:09:31 -0600

    Lab 4 Updates

--- Lab4-BubbleSort/AlexVillegas.js
+++ Lab4-BubbleSort/AlexVillegas.js
@@ -29,21 +29,3 @@ function bubbleSort(arr) {
   console.log("Arreglo desordenado:", arrayToSort);
   const sortedArray = bubbleSort(arrayToSort);
   console.log("Arreglo ordenado:", sortedArray);
-
-  
-/**** Problema: Clasificación de calificaciones de estudiantes **** /
-Imagina que eres profesor en una escuela y has realizado un examen para tus estudiantes.
-Después de calificar sus exámenes, deseas clasificar las calificaciones de mayor a menor
-para tener una mejor idea del rendimiento de la clase.
-
-Utiliza el algoritmo Bubble Sort para ordenar las calificaciones de los estudiantes y determinar las cinco calificaciones más altas.
-
-Instrucciones:
-1. Escribe una función llamada sortGrades que toma un arreglo de calificaciones como argumento y lo ordena de mayor a menor 
-utilizando el algoritmo Bubble Sort.
-
-2. Modifica la función bubbleSort proporcionada anteriormente para ordenar las calificaciones de mayor a menor.
-Crea un arreglo de calificaciones de ejemplo y utilízalo para probar la función sortGrades.
-
-3. Imprime las cinco calificaciones más altas en la consola.
-*/

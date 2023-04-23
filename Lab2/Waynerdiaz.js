a98384ebbcab892d6c5ad92bda45147800abc81b
WaynerDM <morawayner3@gmail.com>
2023-04-22 21:09:13 -0600

    lab2,3,4

--- Lab2/Waynerdiaz.js
+++ Lab2/Waynerdiaz.js
@@ -7,5 +7,5 @@ function linearsearch(arreglo, clave)  {
     }
     return -1;
     }
-    let myArray = [1,2,3,4,5,6,7,8,9,10];
+    let myArray = [1,2,3,4,5,6,7,8,9,10,11];
     console. log("El indice está en la posición:"+ linearSearch (myArray, 2));

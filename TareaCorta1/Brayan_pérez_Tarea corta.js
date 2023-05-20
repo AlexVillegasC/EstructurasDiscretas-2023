f62a943edbc0110a17a7385ae96c798207e5a3a3
Brayanperezballadares <bpballadares@gmail.com>
2023-05-20 14:46:47 -0600

    ....

---
+++ TareaCorta1/Brayan_pérez_Tarea corta.js
@@ -0,0 +1,85 @@
+class Paciente {
+    constructor(nombre, edad, emergencia = false) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.emergencia = emergencia;
+    }
+  }
+  
+  class ColaPacientes {
+    constructor() {
+      this.cola = [];
+    }
+  
+    // Agregar paciente a la cola
+    agregar(paciente) {
+      if (paciente.emergencia) {
+        this.cola.unshift(paciente);
+      } else {
+        this.cola.push(paciente);
+      }
+    }
+  
+    // Eliminar un paciente de la cola
+    eliminar() {
+      if (this.estaVacia()) {
+        return "Cola vacía";
+      }
+      return this.cola.shift();
+    }
+  
+    // Mostrar el paciente al frente de la cola
+    frente() {
+      if (this.estaVacia()) {
+        return "No hay elementos en la cola";
+      }
+      return this.cola[0];
+    }
+  
+    // Mostrar elementos en la cola
+    mostrarCola() {
+      let elementosEnCola = "";
+      for (let i = 0; i < this.cola.length; i++) {
+        elementosEnCola += this.cola[i].nombre + " (" + this.cola[i].edad + " años), ";
+      }
+      return elementosEnCola.slice(0, -2);
+    }
+  
+    // Mostrar el número de pacientes en la cola
+    contar() {
+      return this.cola.length;
+    }
+  
+    // Verificar si la cola está vacía
+    estaVacia() {
+      return this.cola.length === 0;
+    }
+  
+    // Limpiar la cola
+    limpiar() {
+      this.cola = [];
+    }
+  }
+  
+  // Crear una nueva cola de pacientes
+  let colaPacientes = new ColaPacientes();
+  
+  // Registrar pacientes en la cola
+  colaPacientes.agregar(new Paciente("Pepe", 19));
+  colaPacientes.agregar(new Paciente("Esteban", 20));
+  colaPacientes.agregar(new Paciente("Michelle", 24));
+  colaPacientes.agregar(new Paciente("Nicole", 70, true));
+  
+  console.log("Pacientes en la cola: " + colaPacientes.mostrarCola());
+  
+  // Atender pacientes de la cola
+  console.log("Paciente al frente de la cola: " + colaPacientes.frente().nombre);
+  console.log("Atendiendo paciente: " + colaPacientes.eliminar().nombre);
+  console.log("Atendiendo paciente: " + colaPacientes.eliminar().nombre);
+  
+  // Mostrar el número de pacientes restantes en la cola
+  console.log("Número de pacientes restantes en la cola: " + colaPacientes.contar());
+  
+  // Limpiar la cola
+  colaPacientes.limpiar();
+  console.log("Número de pacientes restantes en la cola después de limpiarla: " + colaPacientes.contar());

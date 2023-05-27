0d723b73f29419bab5a6e06e3cfbc0334037698c
Sam2999 <albertsam29@gmail.com>
2023-05-27 15:04:29 -0600

    Sam Quiros Tarea Corta 1

--- TareaCorta1/SamuelQuiros.js
+++ TareaCorta1/SamuelQuiros.js
@@ -1,55 +1,66 @@
 class Paciente {
-    constructor(nombre, edad, emergencia = false) {
-      this.nombre = nombre;
-      this.edad = edad;
-      this.emergencia = emergencia;
-    }
+  constructor(nombre, edad, emergencia = false) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.emergencia = emergencia;
   }
-  
-  class PacienteQueue {
-    constructor() {
-      this.queue = [];
-    }
-  
-    enqueue(paciente) {
-      if (paciente.emergencia) {
-        this.queue.unshift(paciente);
-      } else {
+}
+
+class PacienteQueue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(paciente) {
+    if (this.isEmpty()) {
+      this.queue.push(paciente);
+    } else {
+      let inserted = false;
+      for (let i = 0; i < this.queue.length; i++) {
+        if (paciente.emergencia && !this.queue[i].emergencia) {
+          this.queue.splice(i, 0, paciente);
+          inserted = true;
+          break;
+        }
+      }
+      if (!inserted) {
         this.queue.push(paciente);
       }
     }
-  
-    dequeue() {
-      if (this.isEmpty()) {
-        return null; 
-      }
-      return this.queue.shift(); 
-    }
-  
-    count() {
-      return this.queue.length; 
-    }
-  
-    clear() {
-      this.queue = []; 
-    }
-  
-    isEmpty() {
-      return this.queue.length === 0; 
-    }
   }
-  
-  let pacienteQueue = new PacienteQueue();
-  
-  pacienteQueue.enqueue(new Paciente("Juan", 30));
-  pacienteQueue.enqueue(new Paciente("María", 45, true));
-  
-  const SiguientePaciente = pacienteQueue.dequeue();
-  console.log(SiguientePaciente);
-  
-  const TotalPacientes = pacienteQueue.count();
-  console.log(TotalPacientes); 
-  
-  pacienteQueue.clear();
-  const isEmpty = pacienteQueue.isEmpty();
-  console.log(isEmpty); 
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null; 
+    }
+    return this.queue.shift(); 
+  }
+
+  count() {
+    return this.queue.length; 
+  }
+
+  clear() {
+    this.queue = []; 
+  }
+
+  isEmpty() {
+    return this.queue.length === 0; 
+  }
+}
+
+let pacienteQueue = new PacienteQueue();
+
+pacienteQueue.enqueue(new Paciente("Jerry", 30, false));
+pacienteQueue.enqueue(new Paciente("Marta", 45, true));
+pacienteQueue.enqueue(new Paciente("Jasmin", 18, true));
+
+const siguientePaciente = pacienteQueue.dequeue();
+console.log(siguientePaciente);
+
+const totalPacientes = pacienteQueue.count();
+console.log(totalPacientes);
+
+pacienteQueue.clear();
+const isEmpty = pacienteQueue.isEmpty();
+console.log(isEmpty);
